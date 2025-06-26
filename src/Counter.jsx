a31d84215cbb032c17310332321a684bbc053d6d
Rohit Kumar <rohitrbs2001@gmail.com>
2025-06-26 15:21:49 +0530

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+
+export default function Counter() {
+
+    let [count, setCount] = useState(0);
+
+    let incrCount = () => {
+        count = count + 1;
+        setCount(count);
+        
+    }
+
+    let reset = () =>{
+        setCount(0);
+    }
+    return (
+        <>
+            <h4>Count = {count}</h4>
+            <button onClick={incrCount}>Increase Count</button>
+            <button onClick={reset}>Reset</button>
+        </>
+    );
+}
